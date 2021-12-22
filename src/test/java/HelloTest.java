0217a36ec3d4a1e0cbb41be9f6e7f058455eea6a
Okuducu4609 <okuducu4609@gmail.com>
2021-12-22 18:11:03 +0100

    started hello feature

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -1,6 +1,11 @@
 public class HelloTest {
     public static void main(String[] args) {
-        System.out.println("Hello Test");
+
+        System.out.println("Hello");
+
+        System.out.println("DEveloping hello feature");
+        System.out.println("Local REpo is already published to GitHub");
+
 
     }
 }
